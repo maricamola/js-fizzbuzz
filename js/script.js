2551a2bb804c77a2fb6224298a72eefb36db4fb4
maricamola <maricamola98@libero.it>
2023-03-22 15:26:02 +0100

    createElement "div" with class "box"

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ const container = document.querySelector('.container');
 console.log(container)
 for(let i = 1; i <= 100; i++){
   console.log(i)
+//Creo un "box" ad ogni ciclo
+const box = document.createElement('div')
+//Adesso assegniamo una class al nostro box
+box.classList.add('box')
+//Se multiplo di 3 stampa "Fizz"
+
 }
