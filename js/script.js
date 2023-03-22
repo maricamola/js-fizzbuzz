ff8a74238f4530b4093a4efecfeba426dc8b4e45
maricamola <maricamola98@libero.it>
2023-03-22 18:25:19 +0100

    cycle concluded with multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -6,25 +6,20 @@ for(let i = 1; i <= 100; i++){
   console.log(i)
 //Creo un "box" ad ogni ciclo
 const box = document.createElement('div')
-
 //Adesso assegniamo una class al nostro box
 box.classList.add('box')
-
 //Se multiplo di 3 stampa "Fizz"
-if(!(i % 3) && i) {
+if (!(i % 15)) {
   box.classList.add('rose')
-}else if (!(i % 5) && i){
+}else if (!(i % 3) && i){
   box.classList.add('orange')
-}else if (!(i % 3) && (i % 5)) {
+}else if (!(i % 5) && i){
   box.classList.add('yellow')
 }
-
 //Scriviamo il testo nell'InnerHTML
 box.append(i)
 container.append(box)
 }
-
-
 
 //box.append('Fizz')
 //box.append('Buzz')
