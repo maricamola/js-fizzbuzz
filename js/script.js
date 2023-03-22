2da62e3a843b80426567736f0a803c1b32097955
maricamola <maricamola98@libero.it>
2023-03-22 17:51:06 +0100

    "else if" condition changed

--- js/script.js
+++ js/script.js
@@ -2,28 +2,35 @@
 const container = document.querySelector('.container');
 console.log(container)
 
-
 for(let i = 1; i <= 100; i++){
   console.log(i)
 //Creo un "box" ad ogni ciclo
 const box = document.createElement('div')
 
-
 //Adesso assegniamo una class al nostro box
 box.classList.add('box')
-
 
 //Se multiplo di 3 stampa "Fizz"
 if(!(i % 3) && i) {
   box.classList.add('rose')
-  //Scriviamo il testo nell'InnerHTML
-  box.append('Fizz')
 }else if (!(i % 5) && i){
   box.classList.add('orange')
-  box.append('Buzz')
-} 
+}else if (!(i % 3) && (i % 5)) {
+  box.classList.add('yellow')
+}
 
-
-
+//Scriviamo il testo nell'InnerHTML
+box.append(i)
 container.append(box)
 }
+
+
+
+//box.append('Fizz')
+//box.append('Buzz')
+
+
+
+
+
+
