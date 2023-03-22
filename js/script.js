b9a54b885a128dc8e9186bbe76ee0876a2cf64f2
maricamola <maricamola98@libero.it>
2023-03-22 16:48:19 +0100

    first cycle: multiples of 3 added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 //Seleziono il con container 
 const container = document.querySelector('.container');
 console.log(container)
+
 for(let i = 1; i <= 100; i++){
   console.log(i)
 //Creo un "box" ad ogni ciclo
 const box = document.createElement('div')
+
+
 //Adesso assegniamo una class al nostro box
 box.classList.add('box')
-//Se multiplo di 3 stampa "Fizz"
 
+
+//Se multiplo di 3 stampa "Fizz"
+if(!(i % 3) && i) {
+  box.classList.add('orange')
+}
+
+//Scriviamo il numero nel box
+box.append(i)
+
+container.append(box)
 }
